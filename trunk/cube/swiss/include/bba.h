5dbdd3b08144e2a12e56bf150717e9bc3353f55d
emukidid@gmail.com <emukidid@gmail.com@dca154b3-a78d-53f1-ef34-dd26c8c4ee7e>
2011-11-11 13:30:39 +0000

    - Add missing BBA files

---
+++ trunk/cube/swiss/include/bba.h
@@ -0,0 +1,11 @@
+#ifndef __BBA_H
+#define __BBA_H
+
+extern char bba_ip[16];
+extern int net_initialized;
+
+void resume_netinit_thread();
+void pause_netinit_thread();
+void init_network_thread();
+
+#endif
