b31083194d1b859371dc0cb61f26ef3d9812c403
emukidid <emukidid@gmail.com>
2011-12-26 03:01:31 +0000

    - Add building of bootable GCM images via batch script

---
+++ cube/swiss/buildgcm.bat
@@ -0,0 +1,4 @@
+..\..\pc\utils\mkisofs.exe -R -J -G ..\..\pc\utils\eltorito-e.hdr -no-emul-boot -b swiss-lz.dol -o swiss(pal).gcm dist
+..\..\pc\utils\mkisofs.exe -R -J -G ..\..\pc\utils\eltorito-u.hdr -no-emul-boot -b swiss-lz.dol -o swiss(ntsc).gcm dist
+..\..\pc\utils\mkisofs.exe -R -J -G ..\..\pc\utils\eltorito-j.hdr -no-emul-boot -b swiss-lz.dol -o swiss(ntsc-j).gcm dist
+pause
--- cube/swiss/makeme.bat
+++ cube/swiss/makeme.bat
@@ -5,4 +5,5 @@ del *.elf
 make
 elf2dol swiss.elf swiss.dol
 d0lz swiss.dol swiss-lz.dol -m
+copy swiss-lz.dol dist
 pause
