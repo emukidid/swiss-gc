c0891fd4150a006a319e283e6ad89a99f7a9180c
emukidid <emukidid@gmail.com>
2012-03-24 00:22:01 +0000

    - Modularise the patch codes to have a common base - IDE-EXI code committed is WIP and probably broken

--- cube/patches/makeHDD.bat
+++ cube/patches/makeHDD.bat
@@ -1,8 +1,15 @@
-powerpc-eabi-as hdd.S -o dvdcode.elf
-doltool -d dvdcode.elf
-doltool -b dvdcode.dol
-doltool -c dvdcode.bin 0x80001800 0x80001800
-bin2s dvdcode.bin > READpatch.s
+del *.o
+mkdir built
+powerpc-eabi-gcc -O0 -c ide-exi\hddread.c
+powerpc-eabi-as base\base.S -o base.o
+powerpc-eabi-ld -o hdd.elf base.o hddread.o 
+del *.o
+doltool -d hdd.elf
+doltool -b hdd.dol
+doltool -c hdd.bin 0x80001800 0x80001800
+bin2s hdd.bin > hdd_final.s
+mv hdd_final.s built
+mv hdd.dol built
 del *.bin
 del *.elf
 pause
