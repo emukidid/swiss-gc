27df6db7a38ec1e7a437646ac128eb1ea999d9df
emukidid@gmail.com <emukidid@gmail.com@dca154b3-a78d-53f1-ef34-dd26c8c4ee7e>
2012-03-27 13:48:58 +0000

    - Fix config issues when file is first created - Update interrupt enabled high level patch codes to properly update the DVDFileInfo struct

--- trunk/cube/swiss/source/patches/dvdread_int.s
+++ trunk/cube/swiss/source/patches/dvdread_int.s
@@ -16,50 +16,35 @@ DVDReadInt:
 	stw		%r7, 16(%sp)
 	
 #Call our code to do the load instead
-
-#	mfmsr	%r7
-#	rlwinm	%r7,%r7,0,17,15
-#	mtmsr	%r7
-	
 	lis		%r7,	0x8000
 	ori		%r7,	%r7,	0x180C
 	mtctr	%r7
 	bctrl
 	
-#	mfmsr	%r7
-#	ori		%r7,%r7,0x8000
-#	mtmsr	%r7
-	
 #update dvdstruct
 
 	li		%r0,	0
-	stw     %r0,	0x00(%r3)
-	stw     %r0,	0x04(%r3)
-	stw     %r0,	0x24(%r3)
-	stw     %r0,	0x2C(%r3)
-	stw     %r0,	0x28(%r3)
-	stw     %r0,	0x0C(%r3)
-
+	stw     %r0,	0x0C(%r3)	# state: DVD_STATE_END (read complete)
+	
 	li		%r0,	1
-	stw     %r0,	0x08(%r3)
-
-#offset
-	lwz     %r0,	0x30(%r3)
-	stw     %r0,	0x10(%r3)
-
-#size
-	#lwz     %r0,	0x2F40(%r7)
-	stw     %r5,	0x14(%r3)
-
-#ptr
-	stw     %r4,	0x18(%r3)
-
-#TransferSize
-	stw     %r5,	0x1C(%r3)
-	stw     %r5,	0x20(%r3)
+	stw     %r0,	0x08(%r3)	# command : 1
+	stw     %r5,	0x14(%r3)	# length: passed in length
+	stw     %r4,	0x18(%r3)	# dest: passed in dest
+	stw     %r5,	0x1C(%r3)	# curTransferSize: passed in length
+	stw     %r5,	0x20(%r3)	# transferredSize: passed in length
+# set current file offset.. if it's empty, then set it first to startAddr then add length to it
+	lwz		%r0,	0x10(%r3)
+	cmpwi	%r0,	0
+	bne		skip_set_base_offset
+	lwz     %r7,	0x30(%r3)
+	stw		%r7,	0x10(%r3)	# offset = startAddr
+skip_set_base_offset:
+	lwz     %r7,	0x10(%r3)	# read the current offset
+	add		%r7, 	%r7, %r5	# offset += length;
+	stw     %r7,	0x10(%r3)	# offset is incremented with the read length
 	
-	mr		%r3,	%r5
-	
+	mr      %r3,	%r5
+
 	lwz		%r7, 16(%sp)
 	lwz     %r0, 8(%sp)
 	mtlr    %r0
