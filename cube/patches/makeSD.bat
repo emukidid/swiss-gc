b99b9c93a0d52c05639809dd839e340da58cbcdb
emukidid <emukidid@gmail.com>
2012-05-25 14:41:55 +0000

    - Commit modified batch file for building the SD patch code.

--- cube/patches/makeSD.bat
+++ cube/patches/makeSD.bat
@@ -2,8 +2,8 @@ del *.o
 mkdir built
 powerpc-eabi-as sdgecko\sd.S -o sd.o
 powerpc-eabi-as base\base.S -o base.o
-powerpc-eabi-gcc -O0 -c base\card.c
-powerpc-eabi-ld -o sd.elf base.o sd.o card.o
+powerpc-eabi-gcc -O2 -c base\card.c
+powerpc-eabi-ld -o sd.elf base.o sd.o card.o --entry 0x80001800 --section-start .text=0x80001800
 del *.o
 doltool -d sd.elf
 doltool -b sd.dol
@@ -12,5 +12,5 @@ bin2s sd.bin > sd_final.s
 mv sd_final.s built
 mv sd.dol built
 del *.bin
-del *.elf
+powerpc-eabi-objdump -D sd.elf > dump.txt
 pause
