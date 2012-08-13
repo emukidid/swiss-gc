82cf3eb1fe3b4b0a3fbb734f4a45fed4d35ef18b
emukidid <emukidid@gmail.com>
2012-08-13 13:49:01 +0000

    - Add commented out DVDGetCommandBlockStatus patch

---
+++ cube/swiss/source/patches/dvdgetcommandblockstatus.s
@@ -0,0 +1,14 @@
+#include "asm.h"
+
+# Return commandBlock->status
+#	s32 DVDGetCommandBlockStatus(DVDCommandBlock* commandBlock);
+#	r3	dvdstruct
+
+.globl DVDGetCommandBlockStatus
+DVDGetCommandBlockStatus:
+	lwz     %r3,	0x0C(%r3)	# just return state
+	blr
+
+.globl DVDGetCommandBlockStatus_length
+   DVDGetCommandBlockStatus_length:
+.long (DVDGetCommandBlockStatus_length - DVDGetCommandBlockStatus)
