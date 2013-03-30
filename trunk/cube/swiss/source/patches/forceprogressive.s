729f5874f14de157f26adaccbebaf9363ddaf9c1
metaradil@gmail.com <metaradil@gmail.com@dca154b3-a78d-53f1-ef34-dd26c8c4ee7e>
2013-03-30 15:36:38 +0000

    - Patch GXGetYScaleFactor.

--- trunk/cube/swiss/source/patches/forceprogressive.s
+++ trunk/cube/swiss/source/patches/forceprogressive.s
@@ -9,11 +9,16 @@ ForceProgressive:
 	stw			%r0, 0 (%r3)
 	lhz			%r0, 8 (%r3)
 	cmpwi		%r0, 480
+	ble			2f
+	lhz			%r0, 6 (%r3)
+	clrrwi		%r0, %r0, 1
+	cmpwi		%r0, 480
 	ble			1f
 	li			%r0, 480
-	sth			%r0, 8 (%r3)
+	sth			%r0, 6 (%r3)
+1:	sth			%r0, 8 (%r3)
 	sth			%r0, 16 (%r3)
-1:	lhz			%r0, 16 (%r3)
+2:	lhz			%r0, 16 (%r3)
 	subfic		%r0, %r0, 480
 	srwi		%r0, %r0, 1
 	sth			%r0, 12 (%r3)
