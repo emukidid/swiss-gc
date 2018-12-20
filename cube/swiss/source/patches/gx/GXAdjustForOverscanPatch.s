062f17cbebd1a5440ef16be455df0d3c1a42c7cf
Extrems <metaradil@gmail.com>
2018-12-20 13:49:04 -0500

    - Improve PAL 60Hz forcing with field rendering.

--- cube/swiss/source/patches/gx/GXAdjustForOverscanPatch.s
+++ cube/swiss/source/patches/gx/GXAdjustForOverscanPatch.s
@@ -8,36 +8,38 @@ GXAdjustForOverscanPatch:
 	stw		%r0, 0 (%r4)
 	lhz		%r8, 6 (%r3)
 	lhz		%r9, 8 (%r3)
-	cmpwi	%r9, 480
-	ble		2f
+	lwz		%r10, 20 (%r3)
+	stw		%r10, 20 (%r4)
+	cmpwi	%r10, 0
+	bne		1f
+	andi.	%r0, %r0, 2
+	beq		2f
+1:	li		%r10, 1
+2:	srw		%r11, %r8, %r10
+	srw		%r12, %r9, %r10
+	cmpwi	%r12, 240
+	ble		4f
 	clrrwi	%r6, %r6, 2
-	sub		%r7, %r9, %r8
-	cmpwi	%r8, 480
+	sub		%r7, %r12, %r11
 	mr		%r8, %r9
-	ble		1f
-	subi	%r7, %r8, 480
-1:	srwi	%r7, %r7, 1
-	add		%r6, %r6, %r7
-2:	slwi	%r7, %r6, 1
+	cmpwi	%r11, 240
+	ble		3f
+	subi	%r7, %r12, 240
+3:	add		%r6, %r6, %r7
+4:	slw		%r7, %r6, %r10
 	mullw	%r10, %r8, %r7
 	divwu	%r10, %r10, %r9
 	sub		%r8, %r8, %r10
-	lwz		%r10, 20 (%r3)
-	cmpwi	%r10, 0
-	bne		3f
-	andi.	%r0, %r0, 2
-	beq		4f
-3:	sub		%r9, %r9, %r6
-4:	sub		%r9, %r9, %r6
+	sub		%r9, %r9, %r7
 	sth		%r8, 6 (%r4)
 	sth		%r9, 8 (%r4)
+	slwi	%r7, %r6, 1
 	lhz		%r8, 12 (%r3)
 	lhz		%r9, 16 (%r3)
 	add		%r8, %r8, %r6
 	sub		%r9, %r9, %r7
 	sth		%r8, 12 (%r4)
 	sth		%r9, 16 (%r4)
-	stw		%r10, 20 (%r4)
 	slwi	%r6, %r5, 1
 	lhz		%r7, 4 (%r3)
 	lhz		%r8, 10 (%r3)
