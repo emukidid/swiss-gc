5756e0b81f3ada06499afe63af27ebfa8e6d0fab
Extrems <metaradil@gmail.com>
2019-01-08 15:10:25 -0500

    - Move VIConfigurePan hook past barrier.

--- cube/swiss/source/patches/vi/VIConfigurePanHook.s
+++ cube/swiss/source/patches/vi/VIConfigurePanHook.s
@@ -4,15 +4,15 @@
 
 .globl VIConfigurePanHook
 VIConfigurePanHook:
-	lhz			%r3, 260 (%r29)
-	add			%r4, %r23, %r25
-	cmpw		%r4, %r3
-	ble			1f
-	subf		%r23, %r25, %r3
-1:	mfmsr		%r3
+	mfmsr		%r3
 	rlwinm		%r4, %r3, 0, 17, 15
 	extrwi		%r3, %r3, 1, 16
 	mtmsr		%r4
+	lhz			%r0, 260 (%r29)
+	add			%r4, %r23, %r25
+	cmpw		%r4, %r0
+	blelr
+	subf		%r23, %r25, %r0
 	blr
 
 .globl VIConfigurePanHook_length
