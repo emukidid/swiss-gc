a3c647f83c8d7167b9d14d3a2f72c7bf3e8249eb
emu_kidid <emukidid@gmail.com>
2019-02-02 18:02:34 +1030

    - Fix comment about igr.dol

--- cube/swiss/source/gui/settings.c
+++ cube/swiss/source/gui/settings.c
@@ -31,7 +31,7 @@ char *tooltips_global[PAGE_GLOBAL_MAX+1] = {
 	"System Language:\n\nSystem language used in games, primarily multi-5 PAL games",
 	"SD/IDE Speed:\n\nThe speed to try and use on the EXI bus for SD Gecko or IDE-EXI devices.\n32 MHz may not work on some SD cards.",
 	 NULL,
-	"In-Game Reset: (B + R + Z + DPad Down)\n\nReboot: Soft-Reset the GameCube\nigr.dol: 0x81300000 igr.dol at the root of SD Gecko",
+	"In-Game Reset: (B + R + Z + DPad Down)\n\nReboot: Soft-Reset the GameCube\nigr.dol: Low mem (< 0x81300000) igr.dol at the root of SD Gecko",
 	"Configuration Device:\n\nThe device that Swiss will use to load and save swiss.ini from.\nThis setting is stored in SRAM and will remain on reboot.",
 	NULL,
 	NULL,
