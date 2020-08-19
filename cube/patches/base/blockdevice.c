emukidid/swiss-gc

commit 5ed5d8e6bb2b73b79e0635a4c634ff8c36cb3799

- Reinstate partial read buffer. - Omit lock for non-disc reads.

cube/patches/ide-exi/hddread.c
@@ -306,6 +306,13 @@ void do_read_disc(void *address, uint32_t length, uint32_t offset, uint32_t sect
 	ata.queue[i].callback = callback;
 	if (ata.items++) return;
 
+	if (sector == ata.last_sector) {
+		--ata.items;
+		memcpy(address, sectorBuf + offset, length);
+		callback(address, length);
+		return;
+	}
+
 	ata_read_queued();
 }
 
@@ -336,10 +343,12 @@ void tc_interrupt_handler(OSInterrupt interrupt, OSContext *context)
 #endif
 
 u32 do_read(void *dst, u32 len, u32 offset, u32 sectorLba) {
+	#if !DMA_READ
 	// Try locking EXI bus
 	if(EXILock && !EXILock(exi_channel, EXI_DEVICE_0, 0)) {
 		return 0;
 	}
+	#endif
 	
 	u32 lba = (offset>>9) + sectorLba;
 	u32 startByte = (offset%SECTOR_SIZE);
@@ -373,8 +382,10 @@ u32 do_read(void *dst, u32 len, u32 offset, u32 sectorLba) {
 		}
 	}
 exit:
+	#if !DMA_READ
 	// Unlock EXI bus
 	if (EXIUnlock) EXIUnlock(exi_channel);
+	#endif
 	return numBytes;
 }
 #endif

cube/patches/sdgecko/sd.c
@@ -222,6 +222,13 @@ void do_read_disc(void *address, uint32_t length, uint32_t offset, uint32_t sect
 	mmc.queue[i].callback = callback;
 	if (mmc.items++) return;
 
+	if (sector == mmc.last_sector) {
+		--mmc.items;
+		memcpy(address, sectorBuf + offset, length);
+		callback(address, length);
+		return;
+	}
+
 	mmc_read_queued();
 }
 
@@ -257,10 +264,12 @@ void tc_interrupt_handler(OSInterrupt interrupt, OSContext *context)
 #endif
 
 u32 do_read(void *dst, u32 len, u32 offset, u32 sectorLba) {
+	#if !ISR_READ
 	// Try locking EXI bus
 	if(EXILock && !EXILock(exi_channel, EXI_DEVICE_0, 0)) {
 		return 0;
 	}
+	#endif
 	
 	u32 lba = (offset>>9) + sectorLba;
 	u32 startByte = (offset%SECTOR_SIZE);
@@ -306,8 +315,10 @@ u32 do_read(void *dst, u32 len, u32 offset, u32 sectorLba) {
 	mmc.next_sector = lba + (1<<lbaShift);
 	#endif
 exit:
+	#if !ISR_READ
 	// Unlock EXI bus
 	if (EXIUnlock) EXIUnlock(exi_channel);
+	#endif
 	return numBytes;
 }
 #endif

commit 0527a33ee27b4149476a0c9d55234bed93117a41

- Fix timing regression after 5ed5d8e.

cube/patches/base/blockdevice.c
@@ -40,7 +40,7 @@ void schedule_read(OSTick ticks)
 		dvd.length -= length;
 		dvd.offset += length;
 
-		schedule_read(OSMicrosecondsToTicks(300));
+		schedule_read(READ_COMMAND_LATENCY);
 	}
 	#else
 	OSCancelAlarm(&read_alarm);
@@ -64,7 +64,7 @@ void perform_read(uint32_t address, uint32_t length, uint32_t offset)
 	dvd.length = length;
 	dvd.offset = offset | *VAR_CURRENT_DISC << 31;
 
-	schedule_read(OSMicrosecondsToTicks(300));
+	schedule_read(READ_COMMAND_LATENCY);
 }
 
 void trickle_read(void)

cube/patches/base/emulator.c
@@ -353,7 +353,7 @@ void di_defer_transfer(uint32_t offset, uint32_t length)
 			#endif
 		}
 
-		uint32_t ticks = OSMicrosecondsToTicks(300);
+		uint32_t ticks = READ_COMMAND_LATENCY;
 		ticks += OSSecondsToTicks(CalculateRawDiscReadTime(offset, length));
 		OSSetAlarm(&command_alarm, ticks, alarm_handler);
 

cube/patches/base/emulator.h
@@ -24,6 +24,8 @@
 #include <stdbool.h>
 #include "dolphin/os.h"
 
+#define READ_COMMAND_LATENCY OSMicrosecondsToTicks(300)
+
 extern OSAlarm bba_alarm;
 extern OSAlarm read_alarm;
 

cube/patches/bba/bba.c
@@ -310,7 +310,7 @@ void perform_read(uint32_t address, uint32_t length, uint32_t offset)
 	dvd.length = length;
 	dvd.offset = offset | *VAR_CURRENT_DISC << 31;
 
-	schedule_read(OSMicrosecondsToTicks(300), true);
+	schedule_read(READ_COMMAND_LATENCY, true);
 }
 
 void trickle_read(void)

cube/patches/bba/tcpip.c
@@ -290,7 +290,7 @@ static void udp_input(bba_page_t page, eth_header_t *eth, ipv4_header_t *ipv4, u
 						dvd.length -= data_size;
 						dvd.offset += data_size;
 
-						schedule_read(OSMicrosecondsToTicks(300), false);
+						schedule_read(READ_COMMAND_LATENCY, false);
 					}
 
 					bba_receive_end(page, data + data_offset, size);

cube/patches/ide-exi/hddread.c
@@ -9,6 +9,7 @@
 #include "../base/common.h"
 #include "../base/dolphin/exi.h"
 #include "../base/dolphin/os.h"
+#include "../base/emulator.h"
 
 // NOTE: cs0 then cs1!
 // ATA registers address        val  - cs0 cs1 a2 a1 a0
@@ -285,6 +286,27 @@ static void ata_read_queued(void)
 	uint32_t sector = ata.queue[0].sector;
 
 	ataReadSector(sector, sectorBuf, 0);
+
+	ata.last_sector = sector;
+}
+
+static void ata_done_queued(void)
+{
+	void *address = ata.queue[0].address;
+	uint32_t length = ata.queue[0].length;
+	uint32_t offset = ata.queue[0].offset;
+	uint32_t sector = ata.queue[0].sector;
+	read_frag_cb callback = ata.queue[0].callback;
+
+	if (address != VAR_SECTOR_BUF + offset)
+		memcpy(address, sectorBuf + offset, length);
+
+	if (--ata.items) {
+		memcpy(ata.queue, ata.queue + 1, ata.items * sizeof(*ata.queue));
+		ata_read_queued();
+	}
+
+	callback(address, length);
 }
 
 void do_read_disc(void *address, uint32_t length, uint32_t offset, uint32_t sector, read_frag_cb callback)
@@ -307,9 +329,7 @@ void do_read_disc(void *address, uint32_t length, uint32_t offset, uint32_t sect
 	if (ata.items++) return;
 
 	if (sector == ata.last_sector) {
-		--ata.items;
-		memcpy(address, sectorBuf + offset, length);
-		callback(address, length);
+		OSSetAlarm(&read_alarm, READ_COMMAND_LATENCY, (OSAlarmHandler)ata_done_queued);
 		return;
 	}
 
@@ -323,22 +343,7 @@ void tc_interrupt_handler(OSInterrupt interrupt, OSContext *context)
 	exi_deselect();
 	EXIUnlock(exi_channel);
 
-	void *address = ata.queue[0].address;
-	uint32_t length = ata.queue[0].length;
-	uint32_t offset = ata.queue[0].offset;
-	uint32_t sector = ata.queue[0].sector;
-	read_frag_cb callback = ata.queue[0].callback;
-	ata.last_sector = sector;
-
-	if (address != VAR_SECTOR_BUF + offset)
-		memcpy(address, sectorBuf + offset, length);
-
-	if (--ata.items) {
-		memcpy(ata.queue, ata.queue + 1, ata.items * sizeof(*ata.queue));
-		ata_read_queued();
-	}
-
-	callback(address, length);
+	ata_done_queued();
 }
 #endif
 

cube/patches/sdgecko/sd.c
@@ -9,6 +9,7 @@
 #include "../base/common.h"
 #include "../base/dolphin/exi.h"
 #include "../base/dolphin/os.h"
+#include "../base/emulator.h"
 
 //CMD12 - Stop multiple block read command
 #define CMD12				0x4C
@@ -214,6 +215,28 @@ static void mmc_read_queued(void)
 	}
 
 	rcvr_datablock(sectorBuf, 0, SECTOR_SIZE, 0);
+
+	mmc.last_sector = sector;
+	mmc.next_sector = sector + 1;
+}
+
+static void mmc_done_queued(void)
+{
+	void *address = mmc.queue[0].address;
+	uint32_t length = mmc.queue[0].length;
+	uint32_t offset = mmc.queue[0].offset;
+	uint32_t sector = mmc.queue[0].sector;
+	read_frag_cb callback = mmc.queue[0].callback;
+
+	if (address != VAR_SECTOR_BUF + offset)
+		memcpy(address, sectorBuf + offset, length);
+
+	if (--mmc.items) {
+		memcpy(mmc.queue, mmc.queue + 1, mmc.items * sizeof(*mmc.queue));
+		mmc_read_queued();
+	}
+
+	callback(address, length);
 }
 
 void do_read_disc(void *address, uint32_t length, uint32_t offset, uint32_t sector, read_frag_cb callback)
@@ -236,9 +259,7 @@ void do_read_disc(void *address, uint32_t length, uint32_t offset, uint32_t sect
 	if (mmc.items++) return;
 
 	if (sector == mmc.last_sector) {
-		--mmc.items;
-		memcpy(address, sectorBuf + offset, length);
-		callback(address, length);
+		OSSetAlarm(&read_alarm, READ_COMMAND_LATENCY, (OSAlarmHandler)mmc_done_queued);
 		return;
 	}
 
@@ -256,23 +277,7 @@ void tc_interrupt_handler(OSInterrupt interrupt, OSContext *context)
 	exi_deselect();
 	EXIUnlock(exi_channel);
 
-	void *address = mmc.queue[0].address;
-	uint32_t length = mmc.queue[0].length;
-	uint32_t offset = mmc.queue[0].offset;
-	uint32_t sector = mmc.queue[0].sector;
-	read_frag_cb callback = mmc.queue[0].callback;
-	mmc.last_sector = sector;
-	mmc.next_sector = sector + 1;
-
-	if (address != VAR_SECTOR_BUF + offset)
-		memcpy(address, sectorBuf + offset, length);
-
-	if (--mmc.items) {
-		memcpy(mmc.queue, mmc.queue + 1, mmc.items * sizeof(*mmc.queue));
-		mmc_read_queued();
-	}
-
-	callback(address, length);
+	mmc_done_queued();
 }
 #endif
 

cube/patches/usbgecko/usbgecko.c
@@ -215,7 +215,7 @@ void perform_read(uint32_t address, uint32_t length, uint32_t offset)
 	dvd.length = length;
 	dvd.offset = offset | *VAR_CURRENT_DISC << 31;
 
-	schedule_read(OSMicrosecondsToTicks(300), true);
+	schedule_read(READ_COMMAND_LATENCY, true);
 }
 
 void trickle_read(void)

cube/patches/wkf/wkf.c
@@ -117,7 +117,7 @@ void schedule_read(OSTick ticks)
 		dvd.length -= length;
 		dvd.offset += length;
 
-		schedule_read(OSMicrosecondsToTicks(300));
+		schedule_read(READ_COMMAND_LATENCY);
 	}
 
 	OSCancelAlarm(&read_alarm);
@@ -142,7 +142,7 @@ void perform_read(uint32_t address, uint32_t length, uint32_t offset)
 	dvd.length = length;
 	dvd.offset = offset | *VAR_CURRENT_DISC << 31;
 
-	schedule_read(OSMicrosecondsToTicks(300));
+	schedule_read(READ_COMMAND_LATENCY);
 }
 
 void trickle_read(void)