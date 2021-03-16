2203e89390a9bc8879f830c702aabab3a27649fd
Extrems <metaradil@gmail.com>
2021-03-16 19:17:44 -0400

    - Add game-specific patch for Gun. - Rewrite game-specific patch for Tony Hawk's American Wasteland.

--- cube/swiss/source/patcher.c
+++ cube/swiss/source/patcher.c
@@ -10149,18 +10149,11 @@ int Patch_GameSpecific(void *data, u32 length, const char *gameID, int dataType)
 		switch (length) {
 			case 3420832:
 				// Use debug monitor address instead of 0x81800000/0x83000000.
-				*(u32 *)(data + 0x80014270 - 0x800034A0 + 0x4A0) = 0x3BCD9B18;
-				*(u32 *)(data + 0x80014274 - 0x800034A0 + 0x4A0) = 0x3D208000;
-				*(u32 *)(data + 0x80014278 - 0x800034A0 + 0x4A0) = 0x807E000C;
-				*(u32 *)(data + 0x8001427C - 0x800034A0 + 0x4A0) = 0x38800004;
-				*(u32 *)(data + 0x80014280 - 0x800034A0 + 0x4A0) = 0x83A900EC;
-				*(u32 *)(data + 0x80014284 - 0x800034A0 + 0x4A0) = 0x1C63000C;
-				*(u32 *)(data + 0x800142D8 - 0x800034A0 + 0x4A0) = 0x7FC0E850;
+				*(u32 *)(data + 0x800142C8 - 0x800034A0 + 0x4A0) = 0x3D608000;
+				*(u32 *)(data + 0x800142CC - 0x800034A0 + 0x4A0) = 0x816B00EC;
 				
-				*(u32 *)(data + 0x8001454C - 0x800034A0 + 0x4A0) = 0x3D208000;
-				*(u32 *)(data + 0x80014550 - 0x800034A0 + 0x4A0) = 0x7C7D1B78;
-				*(u32 *)(data + 0x80014554 - 0x800034A0 + 0x4A0) = 0x838900EC;
-				*(u32 *)(data + 0x800145A0 - 0x800034A0 + 0x4A0) = 0x7F83E378;
+				*(u32 *)(data + 0x80014598 - 0x800034A0 + 0x4A0) = 0x3D208000;
+				*(u32 *)(data + 0x8001459C - 0x800034A0 + 0x4A0) = 0x812900EC;
 				
 				print_gecko("Patched:[%.6s]\n", gameID);
 				patched++;
@@ -10535,22 +10528,43 @@ int Patch_GameSpecific(void *data, u32 length, const char *gameID, int dataType)
 				patched++;
 				break;
 		}
+	} else if ((!strncmp(gameID, "GUMD52", 6) || !strncmp(gameID, "GUMP52", 6)) && dataType == PATCH_DOL) {
+		switch (length) {
+			case 3314144:
+				// Use debug monitor address instead of 0x81800000/0x83000000.
+				*(u32 *)(data + 0x8000DD4C - 0x800034A0 + 0x4A0) = 0x3D608000;
+				*(u32 *)(data + 0x8000DD50 - 0x800034A0 + 0x4A0) = 0x816B00EC;
+				
+				*(u32 *)(data + 0x8000DFD4 - 0x800034A0 + 0x4A0) = 0x3C608000;
+				*(u32 *)(data + 0x8000DFD8 - 0x800034A0 + 0x4A0) = 0x806300EC;
+				
+				print_gecko("Patched:[%.6s]\n", gameID);
+				patched++;
+				break;
+		}
+	} else if (!strncmp(gameID, "GUME52", 6) && dataType == PATCH_DOL) {
+		switch (length) {
+			case 3314080:
+				// Use debug monitor address instead of 0x81800000/0x83000000.
+				*(u32 *)(data + 0x8000DD4C - 0x800034A0 + 0x4A0) = 0x3D608000;
+				*(u32 *)(data + 0x8000DD50 - 0x800034A0 + 0x4A0) = 0x816B00EC;
+				
+				*(u32 *)(data + 0x8000DFD4 - 0x800034A0 + 0x4A0) = 0x3C608000;
+				*(u32 *)(data + 0x8000DFD8 - 0x800034A0 + 0x4A0) = 0x806300EC;
+				
+				print_gecko("Patched:[%.6s]\n", gameID);
+				patched++;
+				break;
+		}
 	} else if (!strncmp(gameID, "GWJE52", 6) && dataType == PATCH_DOL) {
 		switch (length) {
 			case 3420512:
 				// Use debug monitor address instead of 0x81800000/0x83000000.
-				*(u32 *)(data + 0x80014270 - 0x800034A0 + 0x4A0) = 0x3BCD9B08;
-				*(u32 *)(data + 0x80014274 - 0x800034A0 + 0x4A0) = 0x3D208000;
-				*(u32 *)(data + 0x80014278 - 0x800034A0 + 0x4A0) = 0x807E000C;
-				*(u32 *)(data + 0x8001427C - 0x800034A0 + 0x4A0) = 0x38800004;
-				*(u32 *)(data + 0x80014280 - 0x800034A0 + 0x4A0) = 0x83A900EC;
-				*(u32 *)(data + 0x80014284 - 0x800034A0 + 0x4A0) = 0x1C63000C;
-				*(u32 *)(data + 0x800142D8 - 0x800034A0 + 0x4A0) = 0x7FC0E850;
+				*(u32 *)(data + 0x800142C8 - 0x800034A0 + 0x4A0) = 0x3D608000;
+				*(u32 *)(data + 0x800142CC - 0x800034A0 + 0x4A0) = 0x816B00EC;
 				
-				*(u32 *)(data + 0x8001454C - 0x800034A0 + 0x4A0) = 0x3D208000;
-				*(u32 *)(data + 0x80014550 - 0x800034A0 + 0x4A0) = 0x7C7D1B78;
-				*(u32 *)(data + 0x80014554 - 0x800034A0 + 0x4A0) = 0x838900EC;
-				*(u32 *)(data + 0x800145A0 - 0x800034A0 + 0x4A0) = 0x7F83E378;
+				*(u32 *)(data + 0x80014598 - 0x800034A0 + 0x4A0) = 0x3D208000;
+				*(u32 *)(data + 0x8001459C - 0x800034A0 + 0x4A0) = 0x812900EC;
 				
 				print_gecko("Patched:[%.6s]\n", gameID);
 				patched++;
