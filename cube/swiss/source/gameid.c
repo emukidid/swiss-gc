77f826835c9fdb5713dafe9d33ffbbab21a4dec9
Extrems <metaradil@gmail.com>
2023-01-10 13:43:54 -0500

    - Broadcast Game ID on all controller sockets.

--- cube/swiss/source/gameid.c
+++ cube/swiss/source/gameid.c
@@ -32,11 +32,12 @@ static void callback(s32 chan, u32 type)
 
 static s32 onreset(s32 final)
 {
-	static bool sent = false;
+	static s32 chan = SI_CHAN0;
 
 	if (!final) {
-		if (!sent) {
-			sent = SI_Transfer(SI_CHAN0, &command, sizeof(command), NULL, 0, callback, 0);
+		if (chan < SI_MAX_CHAN) {
+			if (SI_Transfer(chan, &command, sizeof(command), NULL, 0, callback, 0))
+				chan++;
 			return FALSE;
 		} else if (SI_Busy())
 			return FALSE;
@@ -52,10 +53,12 @@ static sys_resetinfo resetinfo = {
 __attribute((constructor))
 static void gameID_init(void)
 {
-	while (SI_Busy());
+	s32 chan = SI_CHAN0;
 
-	if (SI_Transfer(SI_CHAN0, &command, sizeof(command), NULL, 0, NULL, 0))
-		SI_Sync();
+	while (chan < SI_MAX_CHAN)
+		if (SI_Transfer(chan, &command, sizeof(command), NULL, 0, callback, 0))
+			chan++;
+	while (SI_Busy());
 
 	SYS_RegisterResetFunc(&resetinfo);
 }
